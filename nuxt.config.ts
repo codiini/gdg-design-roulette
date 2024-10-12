7ab936594bcfa69ae5d039d0998f145e538a4694
Iniubong Obonguko <iniubongobonguko2018@gmail.com>
2024-10-12 19:51:39 +0100

    chore: update app title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+  app:{
+    head:{
+      title: 'GDG CodeJam Design Roulette.'
+    }
+  },
   modules: ["@nuxt/ui", "@nuxtjs/supabase"],
   devServer: {
     host: "0.0.0.0",
