ed7782d15ea595b39d8382c59e38dabdef79f74d
Iniubong Obonguko <iniubongobonguko2018@gmail.com>
2024-10-12 18:08:09 +0100

    feat: setup supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ['@nuxt/ui']
-})
+  modules: ["@nuxt/ui", "@nuxtjs/supabase"],
+  devServer: {
+    host: "0.0.0.0",
+  },
+
+  supabase: {
+    // Options
+  },
+});
